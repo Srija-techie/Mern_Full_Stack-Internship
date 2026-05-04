9a10098d15047552e520d7bacba08968141192c5
Srija-techie <srijayarnagula1234@gmail.com>
2026-05-04 21:28:10 +0530

    Add JavaScript files

---
+++ JavaScript/external.js
@@ -0,0 +1,60 @@
+num="java:${10+2}"
+console.log(num);
+num='javascript ${10+20}'
+console.log(num);
+a=20;
+b=30;
+num=`addition of a and b : ${a+b}`
+console.log(num);
+
+
+num="50"
+console.log(num);
+
+num="true"
+console.log(num)
+
+class SampleExample{
+    display(){
+        console.log("This is non-static display methon in SampleExample Class")
+    }
+    test(){
+        console.log("test method");   
+    }
+}
+
+// ternary operator
+num=330;
+result=num%2==0?"Even Number":"Odd Number"
+console.log(result);
+
+// primitive datatypes
+a=10
+b='10';
+c=null
+d=Symbol(10);
+e=2863912639812n
+let f;
+console.log("Type of variable a is:",typeof a);
+console.log("Type of variable b is:",typeof b);
+console.log("Type of variable c is:",typeof c);
+console.log("Type of variable d is:",typeof d);
+console.log("Type of variable e is:",typeof e);
+console.log("Type of variable f is:",typeof f);
+
+// variable
+let a=10;
+a=20;
+let a=20;
+
+
+s1=new SampleExample()
+s1.display()
+s1.test()
+
+function additionExample(){
+    console.log(10+20);
+    
+}
+addition()
+
